8d290f75000c7e4afa74550e13b673f6131f9dac
DUCAN <ducan@mz.co.kr>
2021-08-26 15:47:43 +0700

    add config

--- src/main/java/com/fleta/purchasingservice/PurchasingServiceApplication.java
+++ src/main/java/com/fleta/purchasingservice/PurchasingServiceApplication.java
@@ -1,9 +1,14 @@
 package com.fleta.purchasingservice;
 
+import com.fleta.purchasingservice.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = PurchasingServiceApplication.class)
+@EnableConfigurationProperties({ LiquibaseProperties.class, ApplicationProperties.class })
+
 public class PurchasingServiceApplication {
 
 	public static void main(String[] args) {
